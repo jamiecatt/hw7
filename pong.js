48b674ec1c873ff3374735936b25836c6cefda3d
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-03-06 22:46:14 -0800

    Pong

--- pong.js
+++ pong.js
@@ -5,20 +5,23 @@ var puck = {
   ySpeed: -1,
   r: 15
 };
+
 var edgeOffset = 20;
 
 var player1 = {
   x: edgeOffset,
   y: 200,
   ht: 50,
-  wd: 10
+  wd: 10,
+  score: 0
 };
 
 var player2 = {
-  x: 400-edgeOffset,
+  x: 400 - edgeOffset,
   y: 200,
   ht: 50,
-  wd: 10
+  wd: 10,
+  score: 0
 };
 
 
@@ -28,61 +31,92 @@ function setup() {
 
 function draw() {
   background(255);
-  
-  // draw puck
-  ellipse(puck.x, puck.y, puck.r*2);
-  
+  text("Player 1", 135, 25);
+  text("Player 2", 225, 25);
+  p1ScoreDisplay = text(player1.score, 180, 50);
+  p2ScoreDisplay = text(player2.score, 220, 50);
+    // draw puck
+    ellipse(puck.x, puck.y, puck.r * 2);
+
+
   // move puck
   if (puck.y < puck.r || puck.y > height - puck.r) {
     puck.ySpeed = -puck.ySpeed;
   }
-  
+
   puck.x += puck.xSpeed;
   puck.y += puck.ySpeed;
-  
+
   // draw paddles
   rect(player1.x, player1.y, player1.wd, player1.ht);
-  rect(player2.x-player2.wd, player2.y, player2.wd, player2.ht);
-  
+  rect(player2.x - player2.wd, player2.y, player2.wd, player2.ht);
+
   // paddle movement
-  if (player1.paddleDown && ! player1.paddleUp) {
+  if (player1.paddleDown && !player1.paddleUp) {
     player1.y += 3;
   }
-  if (player1.paddleUp && ! player1.paddleDown) {
+  if (player1.paddleUp && !player1.paddleDown) {
     player1.y -= 3;
-  } 
+  }
 
-  if (player2.paddleDown && ! player2.paddleUp) {
+  if (player2.paddleDown && !player2.paddleUp) {
     player2.y += 3;
   }
-  if (player2.paddleUp && ! player2.paddleDown) {
+  if (player2.paddleUp && !player2.paddleDown) {
     player2.y -= 3;
   }
-  
+
   // don't let paddles outside of the play area
-  player1.y = constrain(player1.y, 0, height-player1.ht-1);
-  player2.y = constrain(player2.y, 0, height-player2.ht-1);
-  
+  player1.y = constrain(player1.y, 0, height - player1.ht - 1);
+  player2.y = constrain(player2.y, 0, height - player2.ht - 1);
+
   // bounce puck on paddles -- player 1 -- based on x-coordinate
   if (puck.x - puck.r < player1.x + player1.wd) {
     // check if puck is within paddle height...
     if (puck.y > player1.y && puck.y < player1.y + player1.ht) {
       puck.xSpeed = abs(puck.xSpeed);
     } else {
-      // ???
+      player2.score += 1;
+      puck.x = 200;
+      puck.y = 200;
+      puck.xSpeed = 1;
+      puck.ySpeed = -1;
+      puck.r = 15;
+      
     }
   }
-  
+
+    //reset score
+  if (player1.score == 10) {
+    puck.xSpeed = 0
+    puck.ySpeed = 0
+  	background(255);
+    textAlign(CENTER);
+    text("Player 1 Wins! \nFinal Score: " + player1.score + " : " + player2.score, width/2, height/2);
+  } else if (player2.score == 10) {
+    puck.xSpeed = 0
+    puck.ySpeed = 0
+  	background(255);
+    textAlign(CENTER);
+    text("Player 2 Wins! \nFinal Score: " + player1.score + " : " + player2.score, width/2, height/2);
+  }
+
   // bounce puck on paddles -- player 2 -- based on x-coordinate
   if (puck.x + puck.r > player2.x - player2.wd) {
     // check if puck is within paddle height...
     if (puck.y > player2.y && puck.y < player2.y + player2.ht) {
       puck.xSpeed = -abs(puck.xSpeed);
     } else {
-      // ???
+      player1.score += 1;
+      puck.x = 200;
+      puck.y = 200;
+      puck.xSpeed = 1;
+      puck.ySpeed = -1;
+      puck.r = 15;
     }
   }
 }
+
 
 // keyboard input
 function keyPressed() {
@@ -92,7 +126,7 @@ function keyPressed() {
   } else if (key == 'Q') {
     player1.paddleUp = true;
   }
-  
+
   if (keyCode == DOWN_ARROW) {
     player2.paddleDown = true;
   } else if (keyCode == UP_ARROW) {
@@ -106,7 +140,7 @@ function keyReleased() {
   } else if (key == 'Q') {
     player1.paddleUp = false;
   }
-  
+
   if (keyCode == DOWN_ARROW) {
     player2.paddleDown = false;
   } else if (keyCode == UP_ARROW) {
